deb3514b45ec16cbb5665616053d6dabf8ed2dc3
Sal-ind <141609771+Sal-ind@users.noreply.github.com>
2023-10-30 12:25:16 +0530

    Add files via upload

--- numpy.py
+++ numpy.py
@@ -1,4 +1,17 @@
+# Python program for
+# Creation of Arrays
 import numpy as np
-arr = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12])
-newarr = arr.reshape(4, 3)
-print(newarr)
+ 
+# Creating a rank 1 Array
+arr = np.array([1, 2, 3])
+print(" \n",arr)
+ 
+# Two diamensional array
+arr = np.array([[1, 2],
+                [3,4]])
+print(" \n", arr)
+ 
+# three dimensional array
+arr = np.array([[1, 2, 3],[4,5,6],[8,9,1],[0,3,4]])
+print("\nArray created using "
+      "passed tuple:\n", arr)
